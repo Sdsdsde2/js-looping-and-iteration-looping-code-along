b07b526bb455a5ee1481442807a8d15e052c7441
Sdsdsde2 <imspammails31@gmail.com>
2021-04-05 19:57:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+let messages = []
+function writeCards(names, gift) {
+    for(let i = 0; i < names.length; i++) {
+        messages.push("Thank you, " + names[i] + ", for the wonderful " + gift + " gift!")
+    }
+    return messages
+}
+
+function countDown(number) {
+    for(let i = 0; i <= number; i++){
+        console.log(i)
+    }
+}
